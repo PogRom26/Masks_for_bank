3c3572747520cd3fe2b70ebb6b14c7cd6d74f11b
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-05 16:50:59 +0700

    add for print card type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-print(get_mask_card_number("Visa Classic 7000792289606361"))
+print(get_mask_card_number("MasterCard 7158300734726758"))
 
 print(get_mask_account("Счет 73654108430135874305"))
--- src/masks.py
+++ src/masks.py
@@ -9,7 +9,14 @@ def get_mask_card_number(card_numbers: str) -> str:
     third_num_quad = card_numbers_masked[-8:-4]
     fours_num_quad = card_numbers_masked[-4:]
 
-    card_numbers_masked_list = [first_num_quad, second_num_quad, third_num_quad, fours_num_quad]
+    # выделение типа карты
+    count_letter = 0
+    for i in card_numbers:
+        if i.isalpha():
+            count_letter +=1
+    card_type = card_numbers[0:count_letter]
+
+    card_numbers_masked_list = [card_type, first_num_quad, second_num_quad, third_num_quad, fours_num_quad]
 
     card_numbers_masked_with_space = " ".join(card_numbers_masked_list)
 

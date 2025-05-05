a5947d667af27b1fbedd28cceebf9bc8ce6e79d1
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-05 16:52:10 +0700

    add for print card type

---
+++ src/widget.py
@@ -0,0 +1,14 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(user_account_or_cart: str, final_info=None) -> str:
+    """Определяет счет или карту и маскирует их номера"""
+
+    if 'Счет' in user_account_or_cart:
+        final_info = get_mask_account(user_account_or_cart)
+    else:
+        final_info = get_mask_card_number(user_account_or_cart)
+
+    return final_info
+
+print(mask_account_card("Maestro 1596837868705199"))

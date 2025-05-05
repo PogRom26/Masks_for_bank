75e8dec2ed7fea39a4053be9041133df06ba5a67
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-05 17:03:34 +0700

    add for print account name

--- src/masks.py
+++ src/masks.py
@@ -16,16 +16,23 @@ def get_mask_card_number(card_numbers: str) -> str:
             count_letter +=1
     card_type = card_numbers[0:count_letter]
 
-    card_numbers_masked_list = [card_type, first_num_quad, second_num_quad, third_num_quad, fours_num_quad]
+    card_numbers_masked_list = [first_num_quad, second_num_quad, third_num_quad, fours_num_quad]
 
     card_numbers_masked_with_space = " ".join(card_numbers_masked_list)
 
-    return card_numbers_masked_with_space
+    return f'{card_type} {card_numbers_masked_with_space}'
 
 
 def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску.
     Видны только последние 4 цифры номера"""
+
+    count_letter = 0
+    for i in account_number:
+        if i.isalpha():
+            count_letter +=1
+    account_type = account_number[0:count_letter]
+
     account_number_masked = len(account_number[-6:-4]) * "*" + account_number[-4:]
 
-    return account_number_masked
+    return f'{account_type} {account_number_masked}'

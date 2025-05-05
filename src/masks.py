0643842927d8dbcd64d40c5bea150fae0ccd959e
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-05 20:53:27 +0700

    upgrade all files after cheking

--- src/masks.py
+++ src/masks.py
@@ -13,14 +13,14 @@ def get_mask_card_number(card_numbers: str) -> str:
     count_letter = 0
     for i in card_numbers:
         if i.isalpha():
-            count_letter +=1
+            count_letter += 1
     card_type = card_numbers[0:count_letter]
 
     card_numbers_masked_list = [first_num_quad, second_num_quad, third_num_quad, fours_num_quad]
 
     card_numbers_masked_with_space = " ".join(card_numbers_masked_list)
 
-    return f'{card_type} {card_numbers_masked_with_space}'
+    return f"{card_type} {card_numbers_masked_with_space}"
 
 
 def get_mask_account(account_number: str) -> str:
@@ -30,9 +30,9 @@ def get_mask_account(account_number: str) -> str:
     count_letter = 0
     for i in account_number:
         if i.isalpha():
-            count_letter +=1
+            count_letter += 1
     account_type = account_number[0:count_letter]
 
     account_number_masked = len(account_number[-6:-4]) * "*" + account_number[-4:]
 
-    return f'{account_type} {account_number_masked}'
+    return f"{account_type} {account_number_masked}"
--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,27 @@
-from operator import index
-
 from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(user_account_or_cart: str, final_info=None) -> str:
     """Определяет счет или карту и маскирует их номера"""
 
-    if 'Счет' in user_account_or_cart:
+    if "Счет" in user_account_or_cart:
         final_info = get_mask_account(user_account_or_cart)
     else:
         final_info = get_mask_card_number(user_account_or_cart)
 
     return final_info
 
+
 def get_date(date: str, date_in_d_m_y=None) -> str:
-    index_T = data_date_time.index("T")
-    date_in_y_m_d = data_date_time[0:index_T]
-    date_in_y_m_d_splited = date_in_y_m_d.split("-")
-    date_in_d_m_y = date_in_y_m_d_splited[2], date_in_y_m_d_splited[1], date_in_y_m_d_splited[0]
+    """принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате
+    "ДД.ММ.ГГГГ"""
+
+    date_in_y_m_d = data_date_time[0 : data_date_time.index("T")]
+    date_in_y_m_d_split = date_in_y_m_d.split("-")
+    date_in_d_m_y = date_in_y_m_d_split[2], date_in_y_m_d_split[1], date_in_y_m_d_split[0]
     date_in_d_m_y_jointed = "-".join(date_in_d_m_y)
     return date_in_d_m_y_jointed
+
 
 data_date_time = "2024-03-11T02:26:18.671407"

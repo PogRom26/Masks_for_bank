089a3bcfa871519693b98f23b5ba468de6c1add8
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-20 21:17:59 +0700

    add func filter_by_state and doing comment

--- src/processing.py
+++ src/processing.py
@@ -16,11 +16,11 @@ def sort_by_date():
     pass
 
 # Для примера работы функции filter_by_state
-list_with_dictionaries = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
- {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
- {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
- {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-new_list = filter_by_state(list_with_dictionaries, 'CANCELED')
-print(new_list)
+# list_with_dictionaries = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+#
+# new_list = filter_by_state(list_with_dictionaries, 'CANCELED')
+# print(new_list)
 

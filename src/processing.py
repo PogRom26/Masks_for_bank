a7cbe39a7537e2bf0e8511d5f2f9090c3a0e5143
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-20 22:00:18 +0700

    modify processing.py - clear comment

--- src/processing.py
+++ src/processing.py
@@ -19,16 +19,3 @@ def sort_by_date(list_with_dictions: list, key="EXECUTED") -> list:
 
     return sorted_list
 
-
-# Для примера работы функций
-list_with_dictionaries = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-
-print(filter_by_state(list_with_dictionaries))
-
-print(sort_by_date(list_with_dictionaries))

15148c86a182f1d28dfac6647d670e3489273d7c
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-22 10:23:42 +0700

    update func sort_by_date: update key

--- src/processing.py
+++ src/processing.py
@@ -12,10 +12,11 @@ def filter_by_state(list_with_dictions: list, key="EXECUTED") -> list:
     return list_with_correct_key
 
 
-def sort_by_date(list_with_dictions: list, key="EXECUTED") -> list:
+def sort_by_date(list_with_dictions: list, key=lambda x: x["date"]) -> list:
     """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
     Функция должна возвращать новый список, отсортированный по дате (date)."""
-    sorted_list = sorted(list_with_dictionaries, key=lambda x: x["date"], reverse=True)
+
+    sorted_list = sorted(list_with_dictionaries, key=key, reverse=True)
 
     return sorted_list
 

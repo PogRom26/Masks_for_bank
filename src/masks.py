98c6e99b411b214954812984344fa8f9dfb94502
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-30 16:06:08 +0700

    update masks.py

--- src/masks.py
+++ src/masks.py
@@ -10,11 +10,7 @@ def get_mask_card_number(card_numbers: str) -> str:
     fours_num_quad = card_numbers_masked[-4:]
 
     # выделение типа карты
-    count_letter = 0
-    for i in card_numbers:
-        if i.isalpha():
-            count_letter += 1
-    card_type = card_numbers[0:count_letter+1]
+    card_type = card_numbers[0:-17]
 
     card_numbers_masked_list = [first_num_quad, second_num_quad, third_num_quad, fours_num_quad]
 

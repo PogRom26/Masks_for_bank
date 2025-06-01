455b82eec7768b58b0801d97e030f8bcf21a9ed6
Roman Pogoreltcev <romzes26@yandex.ru>
2025-06-01 21:00:25 +0700

    update src/masks.p and src/test_masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,8 +1,7 @@
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
+from src.masks import get_mask_card_number, get_mask_account
 import pytest
 
-#Тестирование функций с картой
+# Тестирование функций с картой
 @pytest.mark.parametrize(
     "card_info, masked_card_info",
     [
@@ -21,30 +20,17 @@ def test_get_mask_card_number(card_info, masked_card_info):
 
 
 #Тестирование функций со счетом
-def test_up_get_mask_account():
+@pytest.mark.parametrize("account_info, masked_account_info",
+    [
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Счет 736541084301358743051", "Некорректный номер счета. Должно быть 20 цифр"),
+        ("Счет 7365410843013587430", "Некорректный номер счета. Должно быть 20 цифр"),
+        ("", "Номер счета не указан")
+    ],
+)
+
+def test_up_get_mask_account(account_info, masked_account_info):
     """Тестирование правильности маскирования номера карты. Простой способ"""
-
-    assert get_mask_account("Счет 73654108430135874305") == "Счет **4305"
-    assert get_mask_account("Счет 35383033474447895560") == "Счет **5560"
-    assert get_mask_account("Счет 73654108430135874305") == "Счет **4305"
-
-
-@pytest.fixture
-def account_num_example():
-    return "Счет 73654108430135874305"
-
-def test_len_account_number(account_num_example, len_account_number=20):
-    """Тестирование на количество цифр в данных о карте и их расположение в конце строки"""
-
-    total_count_num = 0
-    count_num_from_end = 0
-    for num in account_num_example[-len_account_number:]:
-        if num in "0123456789":
-            count_num_from_end += 1
-
-    for num in account_num_example:
-        if num in "0123456789":
-            total_count_num += 1
-
-    assert total_count_num == len_account_number
-    assert count_num_from_end == len_account_number
+    assert get_mask_account(account_info) == masked_account_info

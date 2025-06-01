7f6667523738a7c806160cf2297c6fc35f4a77ac
Roman Pogoreltcev <romzes26@yandex.ru>
2025-06-01 20:34:31 +0700

    update src/masks.py. Working without number

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,27 +3,15 @@ from src.masks import get_mask_account
 import pytest
 
 #Тестирование функций с картой
-
-
-def test_up_get_mask_card_number():
-    """Тестирование правильности маскирования номера карты. Простой способ"""
-
-    assert get_mask_card_number("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
-    assert get_mask_card_number("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
-    assert get_mask_card_number("Visa Gold 5999414228426353") == "Visa Gold 5999 41** **** 6353"
-
-
-@pytest.fixture
-def card_num_example():
-    return "Visa Platinum 7000792289606361"
-
-
 @pytest.mark.parametrize(
     "card_info, masked_card_info",
     [
         ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
         ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
         ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Visa Gold 599941422842635311", "Некорректный номер карты. Должно быть 16 цифр"),
+        ("Visa Gold 59994142284263", "Некорректный номер карты. Должно быть 16 цифр"),
+        ("","Номер карты не указан")
     ],
 )
 def test_get_mask_card_number(card_info, masked_card_info):
@@ -32,26 +20,7 @@ def test_get_mask_card_number(card_info, masked_card_info):
     assert get_mask_card_number(card_info) == masked_card_info
 
 
-def test_len_card_number(card_num_example, len_card_number=16):
-    """Тестирование на количество цифр в данных о карте и их расположение в конце строки"""
-
-    total_count_num = 0
-    count_num_from_end = 0
-    for num in card_num_example[-len_card_number:]:
-        if num in "0123456789":
-            count_num_from_end += 1
-
-    for num in card_num_example:
-        if num in "0123456789":
-            total_count_num += 1
-
-    assert total_count_num == len_card_number
-    assert count_num_from_end == len_card_number
-
-
 #Тестирование функций со счетом
-
-
 def test_up_get_mask_account():
     """Тестирование правильности маскирования номера карты. Простой способ"""
 

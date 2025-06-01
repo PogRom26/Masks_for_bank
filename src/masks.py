70519e17341c98ee65124f4cd33a6b79996d4c79
Roman Pogoreltcev <romzes26@yandex.ru>
2025-06-01 20:34:31 +0700

    update src/masks.py. Working without number

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,42 @@
-def get_mask_card_number(card_numbers: str) -> str:
+def get_mask_card_number(card_numbers: str, len_card_number = 16) -> str:
     """принимает на вход номер карты и возвращает ее маску
     Видны первые 6 цифр и последние 4 цифры, остальные символы отображаются звездочками
     """
-    card_numbers_masked = card_numbers[-16:-10] + "*" * 6 + card_numbers[-4:]
 
-    first_num_quad = card_numbers_masked[-16:-12]
-    second_num_quad = card_numbers_masked[-12:-8]
-    third_num_quad = card_numbers_masked[-8:-4]
-    fours_num_quad = card_numbers_masked[-4:]
+    #Считаем (проверяем) цифры в конце строки
+    total_count_num = 0
+    count_num_from_end = 0
+    for num in card_numbers[-len_card_number:]:
+        if num in "0123456789":
+            count_num_from_end += 1
 
-    # выделение типа карты
-    card_type = card_numbers[0:-17]
+    # Считаем (проверяем), что всего 16 цифр в строке
+    for num in card_numbers:
+        if num in "0123456789":
+            total_count_num += 1
 
-    card_numbers_masked_list = [first_num_quad, second_num_quad, third_num_quad, fours_num_quad]
+    if total_count_num == 0:
+        return "Номер карты не указан"
 
-    card_numbers_masked_with_space = " ".join(card_numbers_masked_list)
+    elif len_card_number != total_count_num or len_card_number != count_num_from_end:
+        return "Некорректный номер карты. Должно быть 16 цифр"
 
-    return f"{card_type} {card_numbers_masked_with_space}"
+    else:
+        card_numbers_masked = card_numbers[-16:-10] + "*" * 6 + card_numbers[-4:]
+
+        first_num_quad = card_numbers_masked[-16:-12]
+        second_num_quad = card_numbers_masked[-12:-8]
+        third_num_quad = card_numbers_masked[-8:-4]
+        fours_num_quad = card_numbers_masked[-4:]
+
+        # выделение типа карты
+        card_type = card_numbers[0:-17]
+
+        card_numbers_masked_list = [first_num_quad, second_num_quad, third_num_quad, fours_num_quad]
+
+        card_numbers_masked_with_space = " ".join(card_numbers_masked_list)
+
+        return f"{card_type} {card_numbers_masked_with_space}"
 
 
 def get_mask_account(account_number: str) -> str:

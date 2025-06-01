0bd065582119f126ce94e6fa2051f64f5a0059ac
Roman Pogoreltcev <romzes26@yandex.ru>
2025-06-01 13:25:22 +0700

    add test for count numbers in card data

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,13 +1,13 @@
 from src.masks import get_mask_card_number
-
-
-def test_up_get_mask_card_number():
-    assert get_mask_card_number("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
-    assert get_mask_card_number("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
-    assert get_mask_card_number("Visa Gold 5999414228426353") == "Visa Gold 5999 41** **** 6353"
-
-
 import pytest
+
+#
+# def test_up_get_mask_card_number():
+#     """Тестирование правильности маскирования номера карты. Простой способ"""
+#
+#     assert get_mask_card_number("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
+#     assert get_mask_card_number("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
+#     assert get_mask_card_number("Visa Gold 5999414228426353") == "Visa Gold 5999 41** **** 6353"
 
 
 @pytest.fixture
@@ -24,12 +24,24 @@ def card_num_example():
     ],
 )
 def test_get_mask_card_number(card_info, masked_card_info):
+    """Тестирование правильности маскирования номера карты"""
+
     assert get_mask_card_number(card_info) == masked_card_info
 
 
 def test_len_card_number(card_num_example, len_card_number=16):
-    count_num = 0
+    """Тестирование на количество цифр в данных о карте и их расположение в конце строки"""
+
+    total_count_num = 0
+    count_num_from_end = 0
     for num in card_num_example[-16:]:
         if num in "0123456789":
-            count_num += 1
-    assert count_num == len_card_number
+            count_num_from_end += 1
+
+    for num in card_num_example:
+        if num in "0123456789":
+            total_count_num += 1
+
+    assert total_count_num == len_card_number
+    assert count_num_from_end == len_card_number
+

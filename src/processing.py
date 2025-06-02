a380d16d88a556fcedbfd8ff1bfc393158af5cff
Roman Pogoreltcev <romzes26@yandex.ru>
2025-06-02 10:32:44 +0700

    Add test of processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,16 +6,17 @@ def filter_by_state(list_with_dictions: list, key="EXECUTED") -> list:
     list_with_correct_key = []
 
     for diction in list_with_dictions:
-        if diction.get("state") == key:
-            list_with_correct_key.append(diction)
+        if "state" in diction:
+            if diction.get("state") == key:
+                list_with_correct_key.append(diction)
 
     return list_with_correct_key
 
 
-def sort_by_date(list_with_dictions: list, key=lambda x: x["date"], list_with_dictionaries=None) -> list:
+def sort_by_date(list_with_dictions: list, key=lambda x: x["date"], reverse=True) -> list:
     """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
     Функция должна возвращать новый список, отсортированный по дате (date)."""
 
-    sorted_list = sorted(list_with_dictionaries, key=key, reverse=True)
+    sorted_list = sorted(list_with_dictions, key=key, reverse=reverse)
 
     return sorted_list

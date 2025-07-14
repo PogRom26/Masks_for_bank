dfdbdaaeabfd7a044dc5b2117c66ecc0b2c99653
Roman Pogoreltcev <romzes26@yandex.ru>
2025-07-14 13:14:03 +0700

    Первичное создание файлов и модуля.

---
+++ src/reading_trans.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import csv
+import pathlib
+
+def reading_transaction(path_to_file:str) -> list:
+    """Функция получает путь к файлу с транзакциями, определяет тип файла,
+    обрабатывает файл и выдает список словарей с транзакциями"""
+
+    pass

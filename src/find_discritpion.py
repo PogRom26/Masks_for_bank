e6b4cbef78eeb4e9e6ed328f06d7f5418bbf25ec
Roman <romzes26@yandex.ru>
2025-07-15 19:24:42 +0700

    asasas

--- src/find_discritpion.py
+++ src/find_discritpion.py
@@ -1,6 +1,7 @@
-import re
 from typing import Any
-from unicodedata import category
+from pathlib import Path
+import pandas as pd
+
 
 from src.reading_trans import reading_transaction
 
@@ -9,16 +10,16 @@ def process_bank_operations(data:list[dict], categories:list)-> list[Any]:
     Возвращает словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории.
     Категории операций хранятся в поле description"""
 
-    file_address = "/Users/romanpogorelcev/Documents/Pytons PRO/PythonProject/Masks_for_bank/data/transactions_excel.xlsx"
+    # Путь к текущему модулю
+    current_dir = Path(__file__).parent
+    file_address = current_dir.parent / "src" / "transactions_excel.xlsx"
+
     data_with_trans = reading_transaction(file_address)
     categories = ["Перевод организации", "Открытие вклада"]
-
-
 
     #Формирование нового словаря с ключами - названиями категорий.
 
     key = "description"
-    # dict_with_names_category = [item.get(key) for item in data_with_trans]
 
     dict_with_names_category = []
 
@@ -33,6 +34,12 @@ def process_bank_operations(data:list[dict], categories:list)-> list[Any]:
     return dict_with_names_category
 
 
+
+# Путь к текущему модулю
+current_dir = Path(__file__).parent
+
+# Путь к файлу в соседней папке
+file_path = current_dir.parent / "соседняя_папка" / "файл.txt"
 
 
 file_address = "/Users/romanpogorelcev/Documents/Pytons PRO/PythonProject/Masks_for_bank/data/transactions_excel.xlsx"

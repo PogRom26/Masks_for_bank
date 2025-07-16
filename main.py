35e6361602515698f8f82d221e1a3b56d0534967
Roman Pogoreltcev <romzes26@yandex.ru>
2025-07-16 12:50:25 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,75 @@
-from src.masks import get_mask_account, get_mask_card_number
 
-print(get_mask_card_number("MasterCard 7158300734726758"))
+def main():
+    """Отвечает за основную логику проекта и связывает функциональности между собой"""
+    return "   "
 
-print(get_mask_account("Счет 73654108430135874305"))
+print(main())
+
+
+# Приветствие и вопрос
+first_que = ("""
+ Привет! 
+
+ Добро пожаловать в программу работы с банковскими транзакциями. 
+ Выберите необходимый пункт меню:
+ 1. Получить информацию о транзакциях из JSON-файла
+ 2. Получить информацию о транзакциях из CSV-файла
+ 3. Получить информацию о транзакциях из XLSX-файла""")
+
+print(first_que)
+print()
+
+#Определение типа файла, из которого будут обрабатываться операции
+while True:
+    first_answer = input("Введите его номер: ")
+    print()
+    if first_answer in "123" and len(first_answer) == 1:
+
+        if first_answer == "1":
+            print("Для обработки выбран JSON-файл")
+            file_with_operations = "JSON"
+            break
+
+        elif first_answer == "2":
+            print("Для обработки выбран CSV-файл")
+            file_with_operations = "CSV"
+            break
+
+        elif first_answer == "3":
+            print("Для обработки выбран XLSX-файл")
+            file_with_operations = "XLSX"
+            break
+    else:
+        print("Выбрано число не соответствующее пункту. Попробуйте еще раз")
+
+
+
+second_que = """
+Введите статус, по которому необходимо выполнить фильтрацию. 
+Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING """
+
+print(second_que)
+print()
+
+while True:
+    second_answer = input("Укажите статус: ")
+    print()
+    if second_answer.lower() in ["executed", "canceled", "pending"]:
+
+        if second_answer.lower() == "executed":
+            key = "EXECUTED"
+            print('Операции отфильтрованы по статусу "EXECUTED"')
+            break
+
+        elif second_answer.lower() == "canceled":
+            key = "CANCELED"
+            print('Операции отфильтрованы по статусу "CANCELED"')
+            break
+
+        elif second_answer.lower() == "pending":
+            key = "PENDING"
+            print('Операции отфильтрованы по статусу "PENDING"')
+            break
+
+    else:
+        print('Укажите корректный фильтр')
--- src/masks.py
+++ src/masks.py
@@ -101,3 +101,8 @@ def get_mask_account(account_number: str, len_account_number=20) -> str:
 
         logger.info("Номер счёта замаскирован")
         return f"{account_type} {account_number_masked}"
+
+
+print(get_mask_card_number("MasterCard 7158300734726758"))
+
+print(get_mask_account("Счет 73654108430135874305"))
